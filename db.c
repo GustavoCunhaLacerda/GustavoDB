11d12dd8e423e36b4688d3ba8104717732be12de
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2021-05-26 21:10:21 -0300

    Adicionando erros de string mais longa que o permitido e id negativo

--- db.c
+++ db.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <stdint.h>
 
-#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_USERNAME_SIZE 32 
 #define COLUMN_EMAIL_SIZE 255
 
 typedef enum {
@@ -15,6 +15,8 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
@@ -31,8 +33,8 @@ typedef enum {
 
 typedef struct  {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct {
@@ -83,7 +85,7 @@ void print_row(Row* row) {
 }
 
 Table* new_table() {
-    Table* table = malloc(sizeof(table));
+    Table* table = malloc(sizeof(Table));
 
     table->num_rows = 0;
     for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
@@ -152,20 +154,40 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+    
+    int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+    
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+    
+    return PREPARE_SUCCESS;
+} 
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-            input_buffer->buffer,
-            "insert %d %s %s",
-            &(statement->row_to_insert.id),
-            statement->row_to_insert.username,
-            statement->row_to_insert.email
-        );
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
         statement->type = STATEMENT_SELECT;
@@ -220,9 +242,6 @@ int main(int argc, char* argv[]) {
     Table* table = new_table();
     InputBuffer* input_buffer = new_input_buffer();
 
-    system("cls || clear");
-    printf("\n*** Welcome to GustavoDB ! ***\n\n");
-
     while (true) {
         print_prompt();
         read_input(input_buffer);
@@ -241,6 +260,12 @@ int main(int argc, char* argv[]) {
         switch (prepare_statement(input_buffer, &statement)) {
         case (PREPARE_SUCCESS):
             break;
+        case (PREPARE_NEGATIVE_ID):
+            printf("ID must be positive.\n");
+            continue;
+        case (PREPARE_STRING_TOO_LONG):
+            printf("String is too long.\n");
+            continue;
         case (PREPARE_SYNTAX_ERROR):
             printf("Syntax error. Could not parse statement.\n");
             continue;
